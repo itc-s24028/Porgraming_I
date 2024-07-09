a31d58efcc792ef7a6e8f85f28307bebc26852a4
itc-s24028 <s24028@std.it-college.ac.jp>
2024-07-09 15:48:08 +0900

    2章の終わり

---
+++ 2-12.py
@@ -0,0 +1,12 @@
+import random
+import my_module
+print(my_module.func(5))
+
+print(random.randint(0, 10))
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(random.choice(alphabet))
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(random.sample(alphabet, 5))
+
---
+++ 2-9.py
@@ -0,0 +1,8 @@
+my_list = [0, 2, 4, 6, 8, 10]
+print(3 in my_list)
+
+my_str = 'This is a pen'
+print('apple' in my_str)
+
+my_list = [0, 2, 4, 6, 8, 10]
+print(3 not in my_list)
